4ce2c605b821b6cb69489d36356cf566e8fe62e8
Hamza Aman <thehamzaaman@gmail.com>
2021-04-06 19:41:13 +0500

    resolver updated

--- src/graphql/resolvers/resolver.js
+++ src/graphql/resolvers/resolver.js
@@ -1,8 +1,13 @@
+const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const validator = require('validator')
 const { JWT_KEY } = require('../../config/keys')
 const User = require('../../models/user')
+const Rating = require('../../models/rating')
+const Case = require('../../models/case')
+const Consultation = require('../../models/consultations')
+const Client = require('../../models/client')
 
 const resolver = {
 
@@ -20,6 +25,7 @@ const resolver = {
     }
     const passwordEncryption = await bcrypt.hash(password, 12)
     const user = new User({
+      _id: new mongoose.Types.ObjectId(),
       firstName,
       lastName,
       email,
@@ -74,21 +80,122 @@ const resolver = {
     return result
   },
 
-  login: async ({ email, password}) => {
-    const user = await User.findOne({ email })
+  createRating: async (args, context) => {
+    const { auth } = context;
+    if (!auth){
+      throw new Error ('unauthorized')
+    }
+    const { overallScore, friendlinessScore, knowledgeScore, feedbackText } = args.input
+    const rating = new Rating({
+      overallScore,
+      friendlinessScore,
+      knowledgeScore,
+      feedbackText,
+      attorneyId: auth
+    })
+    const result = await rating.save()
+    await User.findByIdAndUpdate(auth, {
+      $push: { earnings: [result._id]}
+    })
+    return result
+  },
+
+  createCase: async (args, context) => {
+    const { auth } = context
+    console.log('Con3', auth)
+    if (!auth){
+      throw new Error('unathorized')
+    }
+    const { venueState, legalAdviceDescription, areaOfLaw, consultationConnectionMethod, futureConsultationDateTime } = args.input
+    const cas = new Case({
+      venueState, 
+      legalAdviceDescription, 
+      areaOfLaw, 
+      consultationConnectionMethod, 
+      futureConsultationDateTime, 
+      attorneyId: auth
+    })
+    const result = await cas.save()
+    await User.findByIdAndUpdate(auth, {
+      $push: { cases: [result._id]}
+    })
+    return result
+  },
+
+  createConsultation: async (args) => {
+    const { 
+      connectionMethod,
+      dateTimeOccurred,
+      videoConnectionToken,
+      chatConnectionToken,
+      conversationTranscript,
+      status,
+      clientPrefferedConnectionMethod,
+      purchasedLength,
+      actualLength,
+      createdAt,
+      updatedAt,
+      attorneyId } = args.input
+    const consultation = new Consultation({
+      connectionMethod,
+      dateTimeOccurred,
+      videoConnectionToken,
+      chatConnectionToken,
+      conversationTranscript,
+      status,
+      clientPrefferedConnectionMethod,
+      purchasedLength,
+      actualLength,
+      createdAt,
+      updatedAt,
+      attorneyId
+    })
+    const result = consultation.save()
+    return result
+  },
+
+  createClient: async (args) => {
+    const { firstName, lastName, quickBloxId } = args.input
+    const client = Client({
+      firstName,
+      lastName,
+      quickBloxId
+    })
+    const result = await client.save()
+    return result
+  },
+
+  login: async ({ email, password, mode }) => {
+    let user
+    if (mode === 'cases'){
+      user = await User.findOne({ email })
+      .populate({ path: 'cases', model: 'Case'})
+    } else if (mode === 'ratings'){
+      user = await User.findOne({ email })
+      .populate({ path: 'ratings', model: 'Rating'})
+    } else if (mode === 'consultations'){
+      user = await User.findOne({ email })
+      .populate({ path: 'consultations', model: 'Consultation'})
+    } else {
+      user = await User.findOne({ email })
+      .populate({ path: 'cases', model: 'Case'})
+      .populate({ path: 'ratings', model: 'Rating'})
+      .populate({ path: 'consultations', model: 'Consultation'})
+    }
     if (!user){
       throw new Error ('User does not exist')
     }
     const isEqual = await bcrypt.compare(password, user.password)
     if (!isEqual){
       throw new Error ('Password is incorrect')
-    } 
+    }
     const token = jwt.sign({ userId: user.id , email: user.email }, JWT_KEY);
     return { 
       token,  
       user
     }
-  }
+  },
+
 }
 
 module.exports = resolver
