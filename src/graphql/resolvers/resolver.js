8a6bb4994e4641ffa10706af1cb786f67c50d635
Hamza Aman <thehamzaaman@gmail.com>
2021-04-16 16:20:51 +0500

    returning errors syntax fixed

--- src/graphql/resolvers/resolver.js
+++ src/graphql/resolvers/resolver.js
@@ -57,27 +57,32 @@ const resolver = {
   
   verifyEmail: async({ code }, context) => {
     const { auth } = context
-    if (!auth) {
-      throw new Error ('unauthorized')
+    try {
+      if (!auth) {
+        return Error ('unauthorized')
+      }
+      if (!code) {
+        return Error ('verification code missing')
+      }
+      const result = await User.findOne({ _id: auth })
+      if (result.code != code){
+        return Error('Invalid Code')
+      }
+      const res = await User.findOneAndUpdate({ _id: auth }, {
+        verify: true
+      }, {new: true})
+      return res
+    } 
+    catch (e) {
+      return Error ('something went wrong')
     }
-    if (!code) {
-      throw new Error ('verification code missing')
-    }
-    const result = await User.findOne({ _id: auth })
-    if (result.code != code){
-      throw new Error('Invalid Code')
-    }
-    const res = await User.findOneAndUpdate({ _id: auth }, {
-      verify: true
-    }, {new: true})
-    return res
   },
 
   createUserStep1: async(args, context) => {
     try {
       const { auth } = context
       if (!auth){
-        throw new Error ('unauthorized')
+        return Error ('unauthorized')
       }
       const { firstName, lastName, cellPhone, phoneNumberUsageConsent = false , licenseNumber, state } = args.userInput
       const result = await User.findOneAndUpdate({ _id: auth }, {
@@ -90,187 +95,232 @@ const resolver = {
       }, {new: true})
       return result
     } catch (e) {
-      console.log('Err', e)
-      throw new Error ('Something went wrong')
+      return Error ('Something went wrong')
     }
     
   },
 
   createUserStep2: async(args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { practiceAreas, firmAssociation } = args.userInput
+      const result = await User.findOneAndUpdate({ _id: auth }, {
+        practiceAreas,
+        firmAssociation
+      }, { new: true })
+      return result
     }
-    const { practiceAreas, firmAssociation } = args.userInput
-    const result = await User.findOneAndUpdate({ _id: auth }, {
-      practiceAreas,
-      firmAssociation
-    }, { new: true })
-    return result
+    catch (e) {
+      return Error ('Something went wrong')
+    }
   },
   
   createUserStep3: async(args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { investigations, tos, currentProfessionalResponsibilityInvestigations } = args.userInput
+      const result = await User.findOneAndUpdate({ _id: auth }, {
+        investigations,
+        currentProfessionalResponsibilityInvestigations,
+        tos
+      }, { new: true })
+      return result
     }
-    const { investigations, tos, currentProfessionalResponsibilityInvestigations } = args.userInput
-    const result = await User.findOneAndUpdate({ _id: auth }, {
-      investigations,
-      currentProfessionalResponsibilityInvestigations,
-      tos
-    }, { new: true })
-    return result
+    catch (e) {
+      return Error ('Something went wrong')
+    }
+   
   },
 
   createUserStep4: async(args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { commMethods } = args.userInput
+      if (commMethods.length === 0){
+        throw new Error ('Please select atleast 1 channel of communication')
+      }
+      const result = await User.findOneAndUpdate({ _id: auth }, {
+        commMethods
+      }, { new: true })
+      return result
     }
-    const { commMethods } = args.userInput
-    if (commMethods.length === 0){
-      throw new Error ('Please select atleast 1 channel of communication')
+    catch (e) {
+      return Error ('Something went wrong')
     }
-    const result = await User.findOneAndUpdate({ _id: auth }, {
-      commMethods
-    }, { new: true })
-    return result
   },
 
   createRating: async (args, context) => {
     const { auth } = context;
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { overallScore, friendlinessScore, knowledgeScore, feedbackText } = args.input
+      const rating = new Rating({
+        overallScore,
+        friendlinessScore,
+        knowledgeScore,
+        feedbackText,
+        attorneyId: auth
+      })
+      const result = await rating.save()
+      await User.findByIdAndUpdate({ _id: auth} , {
+        $push: { ratings: [result._id]}
+      })
+      return result
+    } catch (e) {
+      return Error ('Something went wrong')
     }
-    const { overallScore, friendlinessScore, knowledgeScore, feedbackText } = args.input
-    const rating = new Rating({
-      overallScore,
-      friendlinessScore,
-      knowledgeScore,
-      feedbackText,
-      attorneyId: auth
-    })
-    const result = await rating.save()
-    await User.findByIdAndUpdate({ _id: auth} , {
-      $push: { ratings: [result._id]}
-    })
-    return result
   },
 
   createCase: async (args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error('unathorized')
+    try {
+      if (!auth){
+        return Error ('unathorized')
+      }
+      const { venueState, legalAdviceDescription, areaOfLaw, consultationConnectionMethod, futureConsultationDateTime } = args.input
+      const cas = new Case({
+        venueState, 
+        legalAdviceDescription, 
+        areaOfLaw, 
+        consultationConnectionMethod, 
+        futureConsultationDateTime, 
+        attorneyId: auth
+      })
+      const result = await cas.save()
+      await User.findByIdAndUpdate(auth, {
+        $push: { cases: [result._id]}
+      })
+      return result
     }
-    const { venueState, legalAdviceDescription, areaOfLaw, consultationConnectionMethod, futureConsultationDateTime } = args.input
-    const cas = new Case({
-      venueState, 
-      legalAdviceDescription, 
-      areaOfLaw, 
-      consultationConnectionMethod, 
-      futureConsultationDateTime, 
-      attorneyId: auth
-    })
-    const result = await cas.save()
-    await User.findByIdAndUpdate(auth, {
-      $push: { cases: [result._id]}
-    })
-    return result
+    catch (e) {
+      return Error ('Something went wrong')
+    }
+    
   },
 
   createConsultation: async (args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { 
+        connectionMethod,
+        dateTimeOccurred,
+        videoConnectionToken,
+        chatConnectionToken,
+        conversationTranscript,
+        status,
+        clientPrefferedConnectionMethod,
+        purchasedLength,
+        actualLength,
+        createdAt,
+        updatedAt,
+        attorneyId } = args.input
+      const consultation = new Consultation({
+        connectionMethod,
+        dateTimeOccurred,
+        videoConnectionToken,
+        chatConnectionToken,
+        conversationTranscript,
+        status,
+        clientPrefferedConnectionMethod,
+        purchasedLength,
+        actualLength,
+        createdAt,
+        updatedAt,
+        attorneyId
+      })
+      const result = consultation.save()
+      return result
     }
-    const { 
-      connectionMethod,
-      dateTimeOccurred,
-      videoConnectionToken,
-      chatConnectionToken,
-      conversationTranscript,
-      status,
-      clientPrefferedConnectionMethod,
-      purchasedLength,
-      actualLength,
-      createdAt,
-      updatedAt,
-      attorneyId } = args.input
-    const consultation = new Consultation({
-      connectionMethod,
-      dateTimeOccurred,
-      videoConnectionToken,
-      chatConnectionToken,
-      conversationTranscript,
-      status,
-      clientPrefferedConnectionMethod,
-      purchasedLength,
-      actualLength,
-      createdAt,
-      updatedAt,
-      attorneyId
-    })
-    const result = consultation.save()
-    return result
+    catch (e) {
+      return Error ('Something went wrong')
+    }
   },
 
   createClient: async (args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      }
+      const { firstName, lastName, quickBloxId } = args.input
+      const client = Client({
+        firstName,
+        lastName,
+        quickBloxId
+      })
+      const result = await client.save()
+      return result
     }
-    const { firstName, lastName, quickBloxId } = args.input
-    const client = Client({
-      firstName,
-      lastName,
-      quickBloxId
-    })
-    const result = await client.save()
-    return result
+    catch (e) {
+      return Error ('Something went wrong')
+    }
   },
 
   login: async ({ email, password }, context ) => {
     const { res } = context
-    const user = await User.findOne({ email })
-    .populate({ path: 'cases', model: 'Case'})
-    .populate({ path: 'ratings', model: 'Rating'})
-    .populate({ path: 'consultations', model: 'Consultation'}).exec()
-    if (!user){
-      throw new Error ('User does not exist')
+    try {
+      const user = await User.findOne({ email })
+      .populate({ path: 'cases', model: 'Case'})
+      .populate({ path: 'ratings', model: 'Rating'})
+      .populate({ path: 'consultations', model: 'Consultation'}).exec()
+      if (!user){
+        return Error ('User does not exist')
+      }
+      const isEqual = await bcrypt.compare(password, user.password)
+      if (!isEqual){
+        return Error ('Password is incorrect')
+      }
+      const token = jwt.sign({ userId: user.id , email: user.email }, JWT_KEY);
+      res.cookie("toki", token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+      })
+      res.cookie('authenticated', true, {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+      })
+      return { 
+        token,  
+        user
+      }
     }
-    const isEqual = await bcrypt.compare(password, user.password)
-    if (!isEqual){
-      throw new Error ('Password is incorrect')
-    }
-    const token = jwt.sign({ userId: user.id , email: user.email }, JWT_KEY);
-    res.cookie("toki", token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 7
-    })
-    res.cookie('authenticated', true, {
-      maxAge: 1000 * 60 * 60 * 24 * 7
-    })
-    return { 
-      token,  
-      user
-    }
+    catch (e) {
+      return Error ('something went wrong')
+    }  
   },
 
   getData: async (args, context) => {
     const { auth } = context
-    if (!auth){
-      throw new Error ('unauthorized')
-    } 
-    const data = User.findOne({ _id: auth })
-    .populate({ path: 'cases', model: 'Case'})
-    .populate({ path: 'ratings', model: 'Rating'})
-    .populate({ path: 'consultations', model: 'Consultation'})
-    if (!data){
-      throw new Error('User does not exist')
+    try {
+      if (!auth){
+        return Error ('unauthorized')
+      } 
+      const data = User.findOne({ _id: auth })
+      .populate({ path: 'cases', model: 'Case'})
+      .populate({ path: 'ratings', model: 'Rating'})
+      .populate({ path: 'consultations', model: 'Consultation'})
+      if (!data){
+        return Error('User does not exist')
+      }
+      return{
+        data
+      }
     }
-    return{
-      data
+    catch (e) {
+      return Error ('something went wrong')
     }
   },
 }
